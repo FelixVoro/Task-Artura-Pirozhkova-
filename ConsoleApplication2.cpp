0c260e8473a01bdd84ba6099736a63706514eb64
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-14 17:02:49 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, D;
+    cout << "A = ";
+    cin >> a;
+    cout << "B = ";
+    cin >> b;
+    cout << "C = ";
+    cin >> c;
+    D = b^2 - 4 * a * c;
+    cout << "Urovnenie imeet ";
+    if (D > 0) 
+        cout << "2 Kornya";   
+    if (D < 0)
+        cout << "0 Korney"; 
+    if (D == 0)
+        cout << "1 koren'";
+}
